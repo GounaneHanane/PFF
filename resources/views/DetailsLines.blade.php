1ceaa6ea73113d60acc8c21dd5a416b2c4c35484
Amine <gounane.hanane@gmail.com>
2018-04-04 19:26:37 +0200

    General:pourcentage

--- resources/views/DetailsLines.blade.php
+++ resources/views/DetailsLines.blade.php
@@ -1,9 +1,9 @@
 @foreach($details as $details)
     <tr style="color: #2a4f7d;">
         <td class="text-center" style="width: 9.09%">{{$details -> imei}}</td>
-        <td class="text-center" style="width: 9.09%">{{$details -> marque}}</td>
-        <td class="text-center" style="width: 9.09%">{{$details -> model}}</td>
-        <td class="text-center" style="width: 9.09%">{{$details -> created_at}}</td>
+        <td class="text-center" style="width: 9.09%">{{ $details->marque }}</td>
+        <td class="text-center" style="width: 9.09%">{{ $details->model }}</td>
+        <td class="text-center" style="width: 9.09%">{{ $details->AddingDate }}</td>
         <td class="text-center" style="width: 9.09%">{{$details -> type}}</td>
         <td class="text-center" style="width: 9.09%">{{$details -> price}}</td>
         <td class="text-center" style="width: 9.09%"><a class="btn btn-danger" onclick="disableDetail({{$details->id}},{{$details->id_contract}})" > <span class="glyphicon glyphicon-trash edit trash "  ></span></a>
